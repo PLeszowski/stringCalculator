9e41092a9683a248992f01f6cc3d03d9a28869d8
PLeszowski <pleszow@gmail.com>
2017-06-13 20:58:27 +0200

    test TDD

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,6 +2,18 @@
 public class StringCalculator {
 
     int add(String numbers){
-        return 0;
+
+		int result = 0;
+
+
+		String[] numersArray = numbers.split("[^0-9]+");
+
+        if(numbers.length() == 0) {result = 0;}
+        else if(numbers.length() == 1) {result = Integer.parseInt(numbers);}
+        else {result = Integer.parseInt(numersArray[0]) + Integer.parseInt(numersArray[1]);}
+
+
+
+        return result;
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,16 +1,45 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 /**
  * Task:
  * https://github.com/mwhelan/Katas/tree/master/Katas.StringCalculator
  *
  */
-
+@RunWith(JUnitParamsRunner.class)
 public class StringCalculatorTest {
 
+    StringCalculator testedObject;
+
+    @Before
+    public void setUp(){
+        testedObject = new StringCalculator();
+    }
+
+    private Object[][] dataForTestingOutput(){
+		return new Object[][]{
+				{"", 0},
+				{"1", 1},
+				{"1,2", 3},
+				{"1,4", 5},
+				{"1,4,2", 5},
+				{"10000,10000", 20000}
+		};
+	}
+
+    @Parameters(method = "dataForTestingOutput")
     @Test
-    public void testConstructor(){
+    public void shouldReturnProperOutputForGivenInput (String input, int expectedResult){
+    	int result = testedObject.add(input);
+
+    	assertThat(result).isEqualTo(expectedResult);
 
     }
+
 
 }
